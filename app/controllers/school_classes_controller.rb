5811eed47045e6eb9af3c923c7a45c0ee360e182
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-01-16 15:19:02 -0500

    commit tree 5e153e9abb46c96acf65fb14bfe3f4200f0660a8

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,37 @@
+class SchoolClassesController < ApplicationController
+
+  before_action :find_school_class, only: [:show, :edit, :update]
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.new(school_class_params)
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def show
+   
+  end
+
+  def edit
+   
+  end
+
+  def update
+    @school_class.update(school_class_params)
+    redirect_to school_class_path(@school_class)
+  end
+
+  private
+
+  def find_school_class
+    @school_class = SchoolClass.find_by_id(params[:id])
+  end
+
+  def school_class_params(*args)
+    params.require(:school_class).permit(:title, :room_number)
+  end
+end
